cceb0cdc8ab3f2565c507910bf1d465d1c2b27ce
AlejandroZapien <alejandrozapien09@gmail.com>
2023-01-25 13:21:47 -0600

    implement simple express routing

--- rawnode-express.js
+++ rawnode-express.js
@@ -3,8 +3,20 @@ const app = express()
 
 const PORT = process.env.PORT || 8000
 
+app.get('/', (req, res) => {
+    res.send("Home")
+})
+
+app.get('/about', (req, res) => {
+    res.send("About")
+})
+
+app.get('/img/dog.png', (req, res) => {
+    res.send("dog")
+})
+
 app.use((req, res) => {
-    res.send("Hello")
+    res.send("not found")
 })
 
 app.listen(PORT, () => {
